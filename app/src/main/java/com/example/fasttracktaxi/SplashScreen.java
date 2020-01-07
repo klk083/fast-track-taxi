b2cf6b920f1281a213986432a9df7c9524af6432
karoljd <karoljd@stud.ntnu.no>
2020-01-07 22:30:44 +0100

    minor change

--- app/src/main/java/com/example/fasttracktaxi/SplashScreen.java
+++ app/src/main/java/com/example/fasttracktaxi/SplashScreen.java
@@ -7,7 +7,7 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int SPLASH_SCREEN_TIME_OUT=5000;
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
